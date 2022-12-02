ac20680446b47c4443a941144850cf6de501da84
Tsumi3000 <leo.jc@outlook.fr>
2022-12-02 18:22:26 +0100

    reduce code day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,13 +1,9 @@
 
-values = open('input.txt').readlines()
 snacks = [0]
-iter = 0
 
-for ligne in values:
-    if ligne == "\n":
-        snacks.append(0)
-        iter+=1
-    else: snacks[iter] += int(ligne[:-1])
+for ligne in open('input.txt').readlines():
+    if ligne == "\n": snacks.append(0)
+    else: snacks[len(snacks) - 1] += int(ligne)
 
 def stape1(): return sorted(snacks)[len(snacks) - 1]
 
