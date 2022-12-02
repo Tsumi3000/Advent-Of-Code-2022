c103d9bf0234b789f29a0aae2f2aafc1ec43b680
Tsushix <leo.jc@outlook.fr>
2022-12-02 19:06:01 +0100

    optimisation

--- day1/day1.py
+++ day1/day1.py
@@ -3,9 +3,9 @@ snacks = [0]
 
 for ligne in open('input.txt').readlines():
     if ligne == "\n": snacks.append(0)
-    else: snacks[len(snacks) - 1] += int(ligne)
+    else: snacks[-1] += int(ligne)
 
-def stape1(): return sorted(snacks)[len(snacks) - 1]
+def stape1(): return sorted(snacks)[-1]
 
 def stape2(): return sum(sorted(snacks)[-3:])
 
