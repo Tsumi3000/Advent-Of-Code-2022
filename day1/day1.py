5a5ca508ef830be0cf9d6ef5dfffa7d8bbcfdf24
Tsumi3000 <leo.jc@outlook.fr>
2022-12-02 15:35:03 +0100

    optimisation

--- day1/day1.py
+++ day1/day1.py
@@ -1,37 +1,16 @@
 
-file = open('input.txt')
-values = file.readlines()
-file.close()
-dictionnary = {0:0}
+values = open('input.txt').readlines()
+snacks = [0]
 iter = 0
 
 for ligne in values:
     if ligne == "\n":
-        dictionnary[iter] = 0
+        snacks.append(0)
         iter+=1
-    else:
-        add = dictionnary[iter]
-        dictionnary[iter] = (int(ligne[:-1]) + add)
+    else: snacks[iter] += int(ligne[:-1])
 
-def stape1():
-    max = 0
-    for i in dictionnary.keys():
-        if dictionnary[i] > max: max = dictionnary[i]
-    return max
+def stape1(): return sorted(snacks)[len(snacks) - 1]
 
-def stape2():
-    top = {0:0,1:0,2:0}
-    for i in dictionnary.keys():
-        if dictionnary[i] > top[0]:
-            top[2] = top[1]
-            top[1] = top[0]
-            top[0] = dictionnary[i]
-        elif dictionnary[i] > top[1]:
-            top[2] = top[1]
-            top[1] = dictionnary[i]
-        elif dictionnary[i] > top[2]:
-            top[2] = dictionnary[i]
+def stape2(): return sum(sorted(snacks)[-3:])
 
-    somme = 0
-    for i in top.keys(): somme+=top[i]
-    return somme
+print(stape1, stape2())
